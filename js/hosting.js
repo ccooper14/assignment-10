c2cd743ebfba76e0089a67e93fd8727ec906c1ef
ccooper14 <ccoop007@gmail.com>
2015-11-06 23:45:28 -0500

    first commit

---
+++ js/hosting.js
@@ -0,0 +1,28 @@
+// demonstrates DOM image swapping
+
+document.getElementById("go").onclick = change;
+
+function change() {
+
+    var url = "http://christiangcooper.com/public_html/images/"
+
+    // get the current state of the button group -
+    var radioButtons = document.getElementsByName("memory");
+
+    // find the checked button -
+    for ( var i in radioButtons ) {
+        if ( radioButtons[i].checked ) {
+            var checkedButton = radioButtons[i].value;
+            break;
+        }
+    }
+
+	// create a string for the image src location
+    // this works b/c I named my button values to match the image filenames
+	var newSource = url + checkedButton + ".jpg";
+
+	// replace the image using the DOM
+    var image = document.getElementById("theMemory");
+	image.setAttribute( "src", newSource );
+
+}
